29a026da9e60590a4d1caace6c7a42ebfaf627c8
nycwy <stuxy.1129@gmail.com>
2025-10-25 17:36:46 +0530

    Feedback UI

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import { useState } from "react";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  }
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={ feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
+}
+
+export default App
