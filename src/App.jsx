1e4f96eb36be63a52df5dc37fc017a1edca1f80d
nycwy <stuxy.1129@gmail.com>
2025-10-27 21:11:42 +0530

    about route is created using (React Router 6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import { useState } from "react";
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -18,14 +20,22 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   }
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          } />
+
+          <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
