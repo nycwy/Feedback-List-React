5fc3f98eaeae2a7036a64850e6ce692cbd3ebcf6
nycwy <stuxy.1129@gmail.com>
2025-10-29 12:36:26 +0530

    added effects on edit icon(button) using useEffect

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -49,7 +49,13 @@ export const FeedbackProvider = ({ children }) => {
     }
 
     
-    return <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback, editFeedback}}>
+    return <FeedbackContext.Provider value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit
+    }}>
         {children}
     </FeedbackContext.Provider>
 }
