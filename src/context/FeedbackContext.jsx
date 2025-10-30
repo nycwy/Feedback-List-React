0e3cf3761d3a49514c269903ac2c5345afbc9959
nycwy <stuxy.1129@gmail.com>
2025-10-30 16:17:01 +0530

    Can add feedback and it will update mock backend json-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -18,7 +17,7 @@ export const FeedbackProvider = ({ children }) => {
 
     //Fetch Feedback
     const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+        const response = await fetch('https://rateandfeedback.netlify.app/feedback?_sort=id&_order=desc');
         const data = await response.json();
         setFeedback(data);
         setIsLoading(false);
@@ -32,9 +31,16 @@ export const FeedbackProvider = ({ children }) => {
     }
 
     //Add Feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('https://rateandfeedback.netlify.app/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        });
+        const data = await response.json();
+        setFeedback([data, ...feedback]);
     }
 
     //Delete Feedback
